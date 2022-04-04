670bff05ad9dd3409d4ecc63a67ec40b3e64966c
AbdelrhmanAmin <maxhd213@gmail.com>
2022-04-04 14:33:49 +0200

    useState instead of useRef

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -4,53 +4,59 @@ import type { ICard } from '@components/ui'
 import ROUTES from '@constants/routes.json'
 import marketplaceFetcher from 'lib/marketplaceFetcher'
 
-export default function Marketplace({ cards }: { cards: ICard[] }) {
-  const cardsRef = React.useRef(cards)
+export default function Marketplace({
+  cards: serverCards,
+}: {
+  cards: ICard[]
+}) {
+  const [cards, setCards] = React.useState(serverCards)
   const timerRef: any = React.useRef(null)
   const currentPageRef = React.useRef(2)
   const lastCardRef = React.useRef(null)
+  const [, trigger] = React.useReducer((prev) => !prev, false)
   const populateWithDummyCards = () => {
-    const tmp = [...cardsRef.current]
+    const tmp = [...cards]
     const startingIndex = tmp.length
-    cardsRef.current.push(
-      ...new Array(20).fill('').map((card, i) => {
-        card = {
-          id: startingIndex + i + 1,
-          name: 'Loading',
-          isLoading: true,
-          media: '',
-        }
-        return card
-      })
-    )
+    const dummy20Cards = new Array(20).fill('').map((_card, i) => ({
+      id: startingIndex + i + 1,
+      name: 'Loading',
+      isLoading: true,
+      media: '',
+      price: 0,
+      status: 'unknown',
+    }))
+    setCards((prevCards) => [...prevCards, ...dummy20Cards])
     return startingIndex
   }
   const fetchAndHydrate = async (startingIndex: number) => {
     const data = await marketplaceFetcher(currentPageRef.current)
     currentPageRef.current += 1
 
-    const cards = data.characters.results
-    for (let i = 0; i < 20; i++) {
-      cardsRef.current[startingIndex + i] = cards[i]
-    }
+    const resultCards = data.characters.results
+
+    setCards((prev) => {
+      for (let i = 0; i < 20; i++) {
+        prev[startingIndex + i] = resultCards[i]
+      }
+      prev.splice(startingIndex, 20, ...resultCards)
+      return prev
+    })
   }
 
   React.useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([{ isIntersecting, target }]) => {
-        if (isIntersecting) {
-          const startingIndex = populateWithDummyCards()
-          observer.unobserve(target)
-          timerRef.current = setTimeout(() => {
-            fetchAndHydrate(startingIndex)
-          }, 2000)
-        }
+    const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        const startingIndex = populateWithDummyCards()
+        timerRef.current = setTimeout(() => {
+          fetchAndHydrate(startingIndex)
+          trigger()
+        }, 2000)
       }
-    )
+    })
     if (lastCardRef.current) {
       observer.observe(lastCardRef.current)
     }
-  }, [lastCardRef.current])
+  }, [lastCardRef])
 
   React.useEffect(() => {
     return () => {
@@ -68,16 +74,7 @@ export default function Marketplace({ cards }: { cards: ICard[] }) {
         <p className="text-center text-lg mt-4">Scroll to infinity!</p>
       </div>
       <div className="mt-4 grid gap-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
-        {cardsRef.current.map((card, i) => {
-          if (i === cardsRef.current.length - 1) {
-            return (
-              <div ref={lastCardRef} className="w-full h-full" key={card.id}>
-                <LinkItem slug={`${ROUTES.MARKETPLACE.slug}/${card.id}`}>
-                  <Card {...card} />
-                </LinkItem>
-              </div>
-            )
-          }
+        {cards.map((card) => {
           return (
             <LinkItem
               slug={`${ROUTES.MARKETPLACE.slug}/${card.id}`}
@@ -87,6 +84,7 @@ export default function Marketplace({ cards }: { cards: ICard[] }) {
             </LinkItem>
           )
         })}
+        <div ref={lastCardRef} />
       </div>
     </Container>
   )
