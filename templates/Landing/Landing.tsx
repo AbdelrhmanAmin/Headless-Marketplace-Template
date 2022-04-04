3e61c5d7cf66e8733f3b081b8cfa0e227c9301a8
AbdelrhmanAmin <maxhd213@gmail.com>
2022-04-04 13:51:00 +0200

    move landing page to templates

---
+++ templates/Landing/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from '@components/ui'
+
+const LandingPage = () => {
+  return (
+    <Container hasPaddingX>
+      <div className="flex flex-col h-screen text-gray-700 justify-center items-center">
+        <h1 className="text-5xl">Landing Page</h1>
+        <p className="text-2xl">Coming soon...</p>
+      </div>
+    </Container>
+  )
+}
+
+export default LandingPage
